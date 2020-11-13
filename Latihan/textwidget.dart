372628fea385d20c6788cf48ed253f0df01823d4
afwjy16 <afif0071997@gmail.com>
2020-11-13 14:37:43 +0700

    first commit

---
+++ Latihan/textwidget.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MyApp());
+}
+
+// Text Widget
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Aplikasi Hello World"),
+        ),
+        body: Center(
+            child: Container(
+                color: Colors.lightBlue,
+                width: 150,
+                height: 350,
+                child: Text(
+                  "Saya sedang melatih kemampuan flutter menggunakan visual studio code",
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  softWrap: false,
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontStyle: FontStyle.italic,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20),
+                ))),
+      ),
+    );
+  }
+}
---
+++ android/app/src/main/kotlin/com/example/apk_hello_word/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.apk_hello_word
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
